859394c8d4507caa33a8d220bd87616b4cda09fc
Sidharth-R-07 <sidhuram365@gmail.com>
2024-09-26 14:11:16 +0530

    .

--- lib/screens/calender_screen.dart
+++ lib/screens/calender_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:holidays_calender/providers/holiday_provider.dart';
 import 'package:holidays_calender/widgets/custom_calender.dart';
-import 'package:holidays_calender/widgets/show_holiday_bottom_sheet.dart';
 
 class CalenderScreen extends ConsumerWidget {
   const CalenderScreen({super.key});
@@ -10,11 +8,8 @@ class CalenderScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      // ),
       appBar: AppBar(
-        title: const Text('TableCalendar - Basics'),
+        title: const Text('Holiday Calender'),
       ),
       body: const CustomCalender(),
     );
